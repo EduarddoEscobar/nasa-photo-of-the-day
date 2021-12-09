134e9f638521ac9cdb8398fa5ba9aa04167cf61a
Eduardo Escobar <eduarddo.escobar@gmail.com>
2021-12-09 15:48:06 -0500

    Styled the main content of the page

--- src/Components/MainContent/NasaImg.js
+++ src/Components/MainContent/NasaImg.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
+const kf = keyframes`
+
+`;
+
+const StyledDiv = styled.div`
+    background-color: ${props => props.theme.primaryColor};
+    padding: 4% 2%;
+    img{
+        width: 50%;
+    }
+    p{
+        font-size: 1.6rem;
+    }
+
+    @media (max-width: ${props => props.theme.breakPointTablet}){
+        display: flex;
+        img{
+        width: 70%;
+        }
+
+        p{
+            font-size: 1.2rem;
+        }
+    }
+
+    @media (max-width: ${props => props.theme.breakPointMobile}){
+        display: block;
+        img{
+            width: 75%;
+        }
+    }
+`
 export default function NasaImg(props){
     const {src, explanation, title} = props
     return(
-        <div>
+        <StyledDiv>
             <img src={src} alt={title}/>
             <p>{explanation}</p>
-        </div>
+        </StyledDiv>
     )
 }
---
+++ src/Theme/Theme.js
@@ -0,0 +1,7 @@
+export default {
+    primaryColor: '#F5F5F5',
+    secondaryColor: 'FBFBFB',
+    borderColor: '#000000',
+    breakPointTablet: '1265px',
+    breakPointMobile: '768px'
+}
